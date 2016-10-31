755b01a4d031e47aff9b19e5be770fe0675fc548
Dionysis <Dionysis.k22@gmail.com>
2016-10-31 14:50:47 +0000

    user class created, requiring relevant files, link to local database

--- app/models/data_mapper_setup.rb
+++ app/models/data_mapper_setup.rb
@@ -1,6 +1,8 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
 
+require_relative 'user'
+
 
 DataMapper.setup(:default, "postgres://localhost/makersbnb_#{ENV['RACK_ENV']}")
 DataMapper.finalize
