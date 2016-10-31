8bfeba17fe8c55b37d323d4e83953caf27b62f7d
Dionysis <Dionysis.k22@gmail.com>
2016-10-31 15:05:09 +0000

    valid format of email for user model and email can only be registered once

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User
   include DataMapper::Resource
 
   property :id, Serial
-  property :email, String, required: true
+
+  property :email, String, required: true, unique: true,
+    :format   => :email_address
+
+  validates_presence_of :email
 
 end
--- spec/units/user_spec.rb
+++ spec/units/user_spec.rb
@@ -10,4 +10,15 @@ describe User do
     expect(user.attributes).to include(:email)
   end
 
+  it "requires a valid email address" do
+    expect{User.create(email: "makers.com")}.to change(User, :count).by(0)
+  end
+
+  it "email address can only be registered once" do
+    User.create(email: "makers@example.com")
+    expect{User.create(email: "makers@example.com")}.to change(User, :count).by(0)
+  end
+
+
+
 end
