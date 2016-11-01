18c07999d4cb5b0630ea9d4d09de66b9c1bc7d11
Dagmara <daga.sz@hotmail.com>
2016-11-01 14:57:31 +0000

    closes #27

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ require 'dm-postgres-adapter'
 require 'bcrypt'
 
 class User
+
+  attr_accessor :password_confirmation
+  attr_reader :password
+
   include DataMapper::Resource
 
   property :id, Serial
@@ -10,6 +14,27 @@ class User
   property :email, String, required: true, unique: true,
     :format   => :email_address
 
+  property :password_digest, Text, required: true
+
+  validates_confirmation_of :password
+  validates_length_of :password, :min => 6
   validates_presence_of :email
+
+
+  def self.authenticate(email, password)
+    user = first(email: email)
+
+    if user && BCrypt::Password.new(user.password_digest) == password
+      user
+    else
+      nil
+    end
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
 
 end
