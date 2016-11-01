3c3b111c3837b1258a2c4a0f8b95aaab5e326dde
Dionysis <Dionysis.k22@gmail.com>
2016-11-01 14:03:08 +0000

    home page has sign up form

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 feature 'User sign up' do
   scenario 'Users can sign up as a new user' do
     visit '/'
-    expect(page).to have_content('Sign up to MakersBnB')
+    expect(page).to have_content('Welcome to MakersBnB')
+    expect(page).to have_content('Sign up here')
+    expect(page).to have_button('Create account')
   end
 end
