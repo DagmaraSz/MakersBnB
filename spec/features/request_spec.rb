5c501ee72d2a0a3f1ff217727ec092798c540d56
your name <akram.rasikh@gmail.com>
2016-11-03 16:57:53 +0000

    jquery adding button

---
+++ spec/features/request_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+feature 'request button' do
+
+  before(:all) do
+    let(:user) {email: "akram@gmail.com", password: "123456"}
+    sign_up(email: user.email, password: user.password)
+    log_out
+    let(:owner) {email: "bob@gmail.com", password: "123456"}
+    sign_up(email: owner.email, password: owner.password)
+    add_property
+    log_out
+  end
+  scenario 'allows you to request a space' do
+    sign_in(email: user.email, password: user.password)
+    expect{click_button 'Request booking'}.to change(Booking, :count).by(1)
+    expect(page).to have_content("Requested")
+    expect(Booking.last.requester).to eq user.email
+    expect(Booking.last.space.name).to eq 'Makers'
+  end
+
+  scenario 'allows owner to see when a place has been requested' do
+    sign_in(email: owner.email, password: owner.password)
+    #page reference ????
+    expect(page).to have_content("Requested")
+  end
+end
--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,9 +1,9 @@
 
-def sign_up
+def sign_up(email:, password:)
   visit '/'
-  fill_in :email_signup, with: 'tester1@tester.com'
-  fill_in :password_signup, with: 123456
-  fill_in :password_confirmation, with: 123456
+  fill_in :email_signup, with: email#'tester1@tester.com'
+  fill_in :password_signup, with: password#123456
+  fill_in :password_confirmation, with: password#123456
   click_button 'Create account'
 end
 
@@ -28,4 +28,19 @@ def sign_in(email:, password:)
   fill_in :email, with: email
   fill_in :password, with: password
   click_button 'Log in'
+end
+
+def add_property
+  visit 'spaces/new'
+  fill_in 'name', with: 'Makers'
+  fill_in 'address', with: 'Commerical road'
+  fill_in 'postcode', with: 'E15X'
+  fill_in 'price', with: 25
+  fill_in 'day', with: '01.07'
+  click_button "Post property"
+end
+
+def log_out
+  visit '/spaces'
+  click_button "Log out"
 end
