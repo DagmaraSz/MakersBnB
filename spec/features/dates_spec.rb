6f7f89bbea32feb5660233ce655f79cefe60c517
your name <akram.rasikh@gmail.com>
2016-11-03 11:50:55 +0000

    filter by tags & params

--- spec/features/dates_spec.rb
+++ spec/features/dates_spec.rb
@@ -16,3 +16,22 @@ feature 'See existing dates' do
     expect(space.calendars.map(&:day)).to include('01/07')
  end
 end
+
+feature 'filter dates' do
+
+  before(:each) do
+    Space.create(name: 'akram house', calendars: [Calendar.first_or_create(day: '01/08')])
+    Space.create(name: 'Dagmara house', calendars: [Calendar.first_or_create(day: '01/09')])
+  end
+  scenario 'I can filter by date' do
+    sign_up
+    visit '/spaces'
+
+    fill_in :day, with: '01/08'
+    click_button 'filter'
+
+    expect(page).to have_content('akram house')
+    expect(page).not_to have_content('Dagmara house')
+
+  end
+end
