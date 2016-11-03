113b7fbbc551732fe9f10eff3a7562d3cd929fd4
your name <akram.rasikh@gmail.com>
2016-11-03 12:14:59 +0000

    increase test coverage

--- spec/features/create_space_spec.rb
+++ spec/features/create_space_spec.rb
@@ -15,7 +15,7 @@ feature 'create space' do
     fill_in 'address', with: 'liverpool street'
     fill_in 'postcode', with: 'W1 5ux'
     fill_in 'price', with: '350'
-    click_button 'Post property'
+    expect{click_button 'Post Property'}.to change(Space, :count).by(1)
     expect(current_path).to eq '/spaces'
     expect(page).to have_content("liverpool street")
   end
