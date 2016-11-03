268021aebaa40efeb994d643da819e8a405bb876
Dionysis <Dionysis.k22@gmail.com>
2016-11-03 14:39:37 +0000

    links to view and create spaces

--- spec/features/create_space_spec.rb
+++ spec/features/create_space_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 require './app/models/space.rb'
 
-feature 'create space' do
+feature 'spaces' do
 
   let!(:user) do
     User.create(email: 'bnb@makers.com', password: '123456',
                 password_confirmation: '123456')
   end
 
-  scenario "it creates and lists space at home" do
+  scenario "it creates and lists spaces" do
     sign_in(email: user.email, password: user.password)
     visit '/spaces/new'
     fill_in 'name', with: '2 bedroom flat'
@@ -18,5 +18,19 @@ feature 'create space' do
     click_button 'Post property'
     expect(current_path).to eq '/spaces'
     expect(page).to have_content("liverpool street")
+  end
+
+  scenario "/spaces has a link to add a new space" do
+    sign_in(email: user.email, password: user.password)
+    visit '/spaces'
+    click_link 'create_space'
+    expect(current_path).to eq '/spaces/new'
+  end
+
+  scenario "/spaces/new has a link to view all listed spaces" do
+    sign_in(email: user.email, password: user.password)
+    visit '/spaces/new'
+    click_link 'listed_spaces'
+    expect(current_path).to eq '/spaces'
   end
 end
